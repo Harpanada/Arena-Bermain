8d64b885f385d68fe878c1aa7118c090392f2a24
Harpanada <liyovanh@gmail.com>
2025-03-25 12:53:21 +0700

    third

--- script.js
+++ script.js
@@ -3,5 +3,16 @@ const slide  = document.getElementById('slide');
 
 checkBox.addEventListener('click', () => {
   slide.classList.toggle('translate-x-full')
-  
+  slide.classList.remove('hidden')
 })
+
+// document.getElementById("checkBox").addEventListener("change", function() {
+//   const slideMenu = document.getElementById("slide");
+//   if (this.checked) {
+//       slideMenu.classList.remove("-right-full");
+//       slideMenu.classList.add("right-0");
+//   } else {
+//       slideMenu.classList.remove("right-0");
+//       slideMenu.classList.add("-right-full");
+//   }
+// });
