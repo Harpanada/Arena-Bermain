4fd935d3c5ca51de9c71a4146c779d8e240b8737
Harpanada <liyovanh@gmail.com>
2025-03-25 14:12:31 +0700

    new feature (Carousel)

--- script.js
+++ script.js
@@ -1,19 +1,38 @@
+//Fungsi Nav Mobile
 const checkBox = document.getElementById('checkBox');
 const slide  = document.getElementById('slide');
 
 checkBox.addEventListener('click', () => {
-  slide.classList.toggle('translate-x-full')
-slide.classList.add('flex')
-  slide.classList.remove('hidden')
+  slide.classList.toggle('translate-x-full');
+slide.classList.add('flex');
+  slide.classList.remove('hidden');
+});
+
+
+//Fungsi Carousel
+const carousel = document.getElementById('carousel');
+const img = document.querySelectorAll('#carousel img');
+const prev= document.getElementById('prev');
+const next =document.getElementById('next');
+
+let index=0;
+
+function updateCarousel(){
+  carousel.style.transform = `translateX(-${index * 100}%)`;
+}
+
+
+next.addEventListener('click',()=>{
+  index = (index + 1) % img.length;
+ updateCarousel();
 })
 
-// document.getElementById("checkBox").addEventListener("change", function() {
-//   const slideMenu = document.getElementById("slide");
-//   if (this.checked) {
-//       slideMenu.classList.remove("-right-full");
-//       slideMenu.classList.add("right-0");
-//   } else {
-//       slideMenu.classList.remove("right-0");
-//       slideMenu.classList.add("-right-full");
-//   }
-// });
+prev.addEventListener('click',() =>{
+  index = (index - 1 + img.length) % img.length;
+  updateCarousel();
+})
+
+// setInterval(() => {
+//   index = (index + 1) % img.length;
+//   updateCarousel();
+// }, 3000);
